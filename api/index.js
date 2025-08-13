99fbdab30586592533d65c741d972d5e832ef309
Chin Phin Shen <iampx95@gmail.com>
2025-08-13 13:58:47 +0800

    enable CORS

--- api/index.js
+++ api/index.js
@@ -1,3 +1,14 @@
 module.exports = (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
   res.status(200).send("Booking System API is running!");
 };

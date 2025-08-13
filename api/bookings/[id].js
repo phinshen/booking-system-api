3cd26a252bf765ca2505cd0305fde1d590b6ffbb
Chin Phin Shen <iampx95@gmail.com>
2025-08-13 13:57:05 +0800

    enable CORS

--- api/bookings/[id].js
+++ api/bookings/[id].js
@@ -1,6 +1,17 @@
 const pool = require("../../db");
 
 module.exports = async (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
   const { id } = req.query;
   const client = await pool.connect();
 
--- api/bookings/index.js
+++ api/bookings/index.js
@@ -1,6 +1,17 @@
 const pool = require("../../db");
 
 module.exports = async (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
   if (req.method === "GET") {
     const client = await pool.connect();
     try {
